1caecf0e7e403c396d7189b5421e1d15afea3105
liruilr <liruilr@shopee.com>
2022-07-17 10:49:35 +0800

    add a test case

--- src/main/java/com/rui/jenkins/api/SpringJenkinsApplication.java
+++ src/main/java/com/rui/jenkins/api/SpringJenkinsApplication.java
@@ -1,2 +1,25 @@
-package com.rui.jenkins.api;public class SpringJenkinsApplication {
+package com.rui.jenkins.api;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationExtensionsKt;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    @PostConstruct
+    public void intt() {
+        logger.info("Application started ...");
+    }
+
+    public static void main(String[] args) {
+        logger.info("Application executed ... ");
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+    }
 }
